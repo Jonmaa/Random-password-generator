544c32edf20793cd2294c08b373b228d9ebe2408
Jon Marcos Mercadé <104355567+Jonmaa@users.noreply.github.com>
2024-10-03 13:52:27 +0200

    files for the password generator

---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",]
+
+let boxEl = document.querySelector(".innertext")
+let boxElTwo = document.querySelector(".innertext2")
+let btn = document.getElementById("generate-btn")
+let passwordLength = 15
+
+function getRandomChar() {
+   let random = Math.floor(Math.random() * characters.length)
+   return characters[random]
+}
+function generatePassword() {
+   let password = ""
+   let passwordTwo = ""
+   for (let i = 0; i < passwordLength; i++) {
+      password += getRandomChar()
+      passwordTwo += getRandomChar()
+   }
+   boxEl.textContent = password
+   boxElTwo.textContent = passwordTwo
+}
+
+btn.addEventListener("click", generatePassword)
+
+
